0bd9825818f32711f50074b21c4c8449d03926fc
William Truppi <williamt@hotmail.it>
2020-11-20 16:39:15 +0100

    esercizio PARI/DISPARI completato

--- Pari e Dispari/assets/js/main.js
+++ Pari e Dispari/assets/js/main.js
@@ -12,11 +12,43 @@ console.log(userNumber);
  * @returns restituisce una variabile contenente il numero casuale generato 
  */
 function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   rndNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  return rndNumber
+  return rndNumber;
 }
 
+// richiamo la funziona e stampo il numero casuale
 var cpuNumber = getRandomNumber(1, 5);
 console.log(cpuNumber);
+
+//effettuo la somma tra il numero dell'utente e quello generato casualmente
+var sum = cpuNumber + userNumber;
+console.log(sum);
+
+//dichiaro una variabile che conterrà il risultato del gioco
+var message;
+
+/** Funziona che svolge il controllo sul risultato della somma
+ * se pari e utente scegli pari --- ha vinto
+ * se dispari e utente scegli pari --- ha vinto
+ * se dispari e utente scegli pari --- ha perso
+ * se pari e utente scegli disppari --- ha perso
+ * @param {*} sum 
+ * @returns
+ */
+function isEvenOrOdd (sum){
+  if (sum % 2 == 0 && userChoice === "pari"){
+    message = "il risultato è PARI ---> HAI VINTO!!";
+  } else if (sum % 2 != 0 && userChoice === "dispari") {
+    message = "il risultato è DISPARI ---> HAI VINTO!!";
+  } else if (sum % 2 != 0 && userChoice === "pari") {
+    message = "il risultato è DISPARI ---> HAI PERSO!!";
+  } else if (sum % 2 == 0 && userChoice === "dispari") {
+    message = "il risultato è PARI ---> HAI PERSO!!";
+  }
+
+  return message;
+}
+
+// invoco la funzione e stampo il risultato
+var gameResult = isEvenOrOdd (sum);
+console.log(gameResult);
